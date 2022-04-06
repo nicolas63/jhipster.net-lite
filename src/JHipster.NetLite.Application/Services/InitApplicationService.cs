50e24473f146e7c083dee8dada3c1783537dd70a
nicolas63 <nicolas_raymond@orange.fr>
2022-04-06 16:51:50 +0200

    Added logger abstraction

--- src/JHipster.NetLite.Application/Services/InitApplicationService.cs
+++ src/JHipster.NetLite.Application/Services/InitApplicationService.cs
@@ -1,6 +1,7 @@
 ﻿using JHipster.NetLite.Application.Services.Interfaces;
 using JHipster.NetLite.Domain.Entities;
 using JHipster.NetLite.Domain.Services.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace JHipster.NetLite.Application.Services;
 
@@ -8,9 +9,12 @@ public class InitApplicationService : IInitApplicationService
 {
     private readonly IInitDomainService _initDomainService;
 
-    public InitApplicationService(IInitDomainService initDomainService)
+    private readonly ILogger<InitApplicationService> _logger; 
+
+    public InitApplicationService(IInitDomainService initDomainService, ILogger<InitApplicationService> logger)
     {
         _initDomainService = initDomainService;
+        _logger = logger;
     }
 
     public void Init(Project project)
