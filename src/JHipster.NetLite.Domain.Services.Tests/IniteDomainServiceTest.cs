c885700c44a95434c012e190bb0539f0e8e30e49
VIGNON <ugo.vignon@cgi.com>
2022-04-14 11:21:32 +0200

    Unit tests + correction Template and add methods

---
+++ src/JHipster.NetLite.Domain.Services.Tests/IniteDomainServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using JHipster.NetLite.Domain.Entities;
+using JHipster.NetLite.Domain.Repositories.Interfaces;
+using JHipster.NetLite.Domain.Services;
+using JHipster.NetLite.Domain.Services.Interfaces;
+using JHipster.NetLite.Infrastructure.Repositories;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHipster.NetLite.Web.Tests
+{
+    //TODO: j'ai mélangé test repo et test InitDomainService
+    //TODO: faire un projet (dans le dossier tests) pour chaque couche avec les tests qui iront dans leur projet correspodant
+    [TestClass]
+    public class IniteDomainServiceTest
+    {
+        private IInitDomainService DomainService { get; set; }
+        private Project Project { get; set; }
+        public ILogger<InitDomainService> Logger { get; set; } = new NullLogger<InitDomainService>();
+
+        public IniteDomainServiceTest()
+        {
+        }
+
+        [TestInitialize]
+        public async Task InitTest()
+        {
+            Project = new Project(Path.Join(Directory.GetCurrentDirectory(), "Test"));
+            DomainService = new InitDomainService(new ProjectLocalRepository(Logger), Logger);
+        }
+
+        [TestMethod]
+        public async Task Should_NotThrow_When_Init()
+        {
+            //Arrange
+
+            //Act
+            Func<Task> task = async () => await DomainService.Init(Project);
+
+            //Assert
+            await task.Should().NotThrowAsync();
+        }
+
+        
+    }
+}
